bf09f2824b24d3102fbecbc9d802c6620ae1f75c
Mohamed Hesham <125760308+Eitsh@users.noreply.github.com>
2024-10-03 17:30:43 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,80 +16,79 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Row(),
-            const SizedBox(
-              height: 70,
-            ),
-            CircleAvatar(
-              radius: 60,
-              foregroundImage: AssetImage("assets/download.jpeg"),
-            ),
-            const Text(
-              "Hosna",
-            ),
-            const Text(
-              "FLUTTER Developer",
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [Text('Projects'), Text('10')],
-                ),
-                Column(children: [Text('Projects'), Text('200')]),
-                Column(children: [Text('Projects'), Text('33')]),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-                width: 400,
-                child: const TextField(
-                  decoration: InputDecoration(labelText: "username"),
-                )),
-            const SizedBox(),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 400,
-              child: const TextField(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Row(),
+              const SizedBox(
+                height: 70,
+              ),
+              CircleAvatar(
+                radius: 60,
+                foregroundImage: AssetImage("assets/download.jpeg"),
+              ),
+              const Text(
+                "Hosna",
+              ),
+              const Text(
+                "FLUTTER Developer",
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [Text('Projects'), Text('10')],
+                  ),
+                  Column(children: [Text('Likes'), Text('50k')]),
+                  Column(children: [Text('Follwers'), Text('45k')]),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: "username"),
+              ),
+              const SizedBox(),
+              const SizedBox(
+                height: 20,
+              ),
+              const TextField(
                 decoration: InputDecoration(labelText: 'Password'),
                 obscureText: true,
               ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print("wait for sigging in ");
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                backgroundColor: Colors.black,
-                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+              SizedBox(
+                height: 50,
               ),
-              child: const Text(
-                " خش يا مافيا ",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
+              ElevatedButton(
+                onPressed: () {
+                  print("wait for sigging in ");
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  backgroundColor: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                ),
+                child: const Text(
+                  " خش يا مافيا ",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],//ki
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
